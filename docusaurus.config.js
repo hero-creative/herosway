828e81f727b39b3117151429bf37cbb9834c534e
Michael Carr <xyeres@users.noreply.github.com>
2023-09-15 21:56:54 +0000

    add more contextual stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,7 +62,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/herocreative_opengraph_bluefinal4x.png",
       navbar: {
         // title: "The Hero's Way",
         logo: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Helpful guides to help our clients succeed.">
       <HomepageHeader />
       {/* <main>
         <HomepageFeatures />
